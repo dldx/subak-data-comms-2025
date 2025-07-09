f52224369f44df1fdc01d52e05cc287c792b15f6
Durand D'souza <durand1@gmail.com>
2025-07-09 02:30:48 +0100

    Add closing slide with contact information and links to social profiles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -779,4 +779,52 @@
 		</Transition>
 	</Slide>
 
+	<!-- Closing Slide -->
+	<Slide class="place-content-center place-items-center h-full"
+	out={() => {
+				layout = 'h-[70vh]'
+	}}
+		in={() => {
+			layout = ''
+		}}>
+		<Transition visible>
+			<div class="space-y-12 max-w-4xl text-center">
+				<h1 class="font-bold text-white text-6xl">Any Questions?</h1>
+
+				<div class="space-y-8">
+
+					<Transition>
+						<div class="space-y-6">
+							<div class="bg-white/5 mt-6 p-6 border-[var(--cti-tertiary)] border-l-4">
+								<h3 class="mb-4 font-semibold text-[var(--cti-tertiary)] text-2xl">Get in touch</h3>
+								<div class="space-y-2 text-white text-lg">
+									<p>
+										<span class="font-semibold text-2xl">Durand D'souza</span>
+									</p>
+										<a
+											href="https://www.linkedin.com/in/durand1/"
+											class="flex flex-row justify-center items-center gap-2 font-semibold text-[var(--cti-tertiary)] hover:underline no-underline"
+											target="_blank"
+										>
+											<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+										durand1
+										</a>
+										<a
+											href="https://bsky.app/profile/dldx.org"
+											class="flex flex-row justify-center items-center gap-2 font-semibold text-[var(--cti-tertiary)] hover:underline no-underline"
+											target="_blank"
+										>
+											<svg role="img" viewBox="0 0 24 24" fill="currentColor" stroke="none" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><title>Bluesky</title><path d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479.815 2.736 3.713 3.66 6.383 3.364.136-.02.275-.039.415-.056-.138.022-.276.04-.415.056-3.912.58-7.387 2.005-2.83 7.078 5.013 5.19 6.87-1.113 7.823-4.308.953 3.195 2.05 9.271 7.733 4.308 4.267-4.308 1.172-6.498-2.74-7.078a8.741 8.741 0 0 1-.415-.056c.14.017.279.036.415.056 2.67.297 5.568-.628 6.383-3.364.246-.828.624-5.79.624-6.478 0-.69-.139-1.861-.902-2.206-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z"/></svg>
+											dldx.org
+										</a>
+
+								</div>
+							</div>
+						</div>
+					</Transition>
+				</div>
+			</div>
+		</Transition>
+	</Slide>
+
 </Presentation>
